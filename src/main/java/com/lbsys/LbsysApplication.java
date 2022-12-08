51e52fc3054e853f29bea63638458b949b428d66
asuslaptop <491098453@qq.com>
2022-12-08 09:56:52 +0800

    11

---
+++ src/main/java/com/lbsys/LbsysApplication.java
@@ -0,0 +1,13 @@
+package com.lbsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbsys/LbsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
